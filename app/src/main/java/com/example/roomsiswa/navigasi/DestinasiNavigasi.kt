691c2341a20e253f1c6354184c97c944afae58e6
Siti Marfungah Dwi Lestari <siti.marfungah.ft21@mail.umy.ac.id>
2023-12-06 21:57:25 +0700

    membuat destinasi navigasi

--- app/src/main/java/com/example/roomsiswa/navigasi/DestinasiNavigasi.kt
+++ app/src/main/java/com/example/roomsiswa/navigasi/DestinasiNavigasi.kt
@@ -1,4 +1,8 @@
 package com.example.roomsiswa.navigasi
 
 interface DestinasiNavigasi {
+    //    Nama untuk untuk menentukan jalur untuk composable
+    val route:String
+    //    String resource id yang berisi judul yang akan ditampilkan di layar halaman
+    val titleRes: Int
 }
