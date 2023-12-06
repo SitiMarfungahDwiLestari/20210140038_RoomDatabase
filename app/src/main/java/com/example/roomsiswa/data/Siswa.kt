327c6d505acbae57dc196acb09892d49a82cd277
Siti Marfungah Dwi Lestari <siti.marfungah.ft21@mail.umy.ac.id>
2023-12-06 20:58:02 +0700

    membuat data Siswa pada package data

--- app/src/main/java/com/example/roomsiswa/data/Siswa.kt
+++ app/src/main/java/com/example/roomsiswa/data/Siswa.kt
@@ -1,4 +1,15 @@
 package com.example.roomsiswa.data
 
-class Siswa {
-}
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+@Entity(tableName = "tblSiswa")
+data class  Siswa(
+    //tag primary key digunakan untuk menandakan bahwa primary key untuk dibawahnya(jika tag primary key diletakkan di atas id, maka idnya yang jadi primary key. jika diatas nama, maka primary keynya diletakkan diatas nama)
+    @PrimaryKey(autoGenerate = true)
+    val id : Int = 0,
+    val nama : String,
+    val alamat : String,
+    val telpon : String
+)
